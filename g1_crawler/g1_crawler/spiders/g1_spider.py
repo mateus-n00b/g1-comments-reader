ed06ed4b1381d5598979bf4df1c3ec4c692a64f3
mateus-n00b <mateus.aluufc@gmail.com>
2019-11-28 12:51:26 -0300

    Fixing errors

--- g1_crawler/g1_crawler/spiders/g1_spider.py
+++ g1_crawler/g1_crawler/spiders/g1_spider.py
@@ -8,14 +8,18 @@ class g1Spyder(scrapy.Spider):
 
     def start_requests(self):
         # Define your URLs here 
-        urls = ['https://g1.globo.com/rs/rio-grande-do-sul/noticia/2019/11/27/relator-do-processo-sobre-sitio-de-atibaia-no-trf-4-vota-por-condenacao-de-lula-por-corrupcao-e-lavagem-de-dinheiro.ghtml']
-        self.comments_url = "https://comentarios.globo.com/comentarios/{0}/{1}/{2}/shorturl/{3}/"
-        #SETTINGS
+        urls = ['https://g1.globo.com/natureza/noticia/2019/11/28/terras-indigenas-tem-alta-de-74percent-no-desmatamento-area-mais-afetada-protege-povo-isolado.ghtml']
+        # URL for get comments
+        self.comments_url = "https://comentarios.globo.com/comentarios/{0}/{1}/{2}/shorturl/{3}/"        
+        # Iterates for each URL
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
     def parse(self, response):
         try:
+            # Find "#SETTINGS" script that contains the comments ID
             content = response.css('#SETTINGS').get()
+            # Filter the response
+            # The replace('/','@@') is needed for url encode
             external_id = re.search('COMENTARIOS_IDEXTERNO: "(.*)"', content).group(1).replace("/","@@")
             external_id = quote(external_id)
             comments_uri = re.search('COMENTARIOS_URI: \"(.*)\"(,[A-Z])', content).group(1).replace("/","@@")
@@ -27,9 +31,8 @@ class g1Spyder(scrapy.Spider):
         except Exception as err:
             print(err)
             exit(1)
-
-        self.comments_url = self.comments_url.format(comments_uri, external_id, cannonical_url, title)+"numero"
-        
+        # /numero endpoint returns the number of comments in the page.
+        self.comments_url = self.comments_url.format(comments_uri, external_id, cannonical_url, title)+"numero"        
         yield response.follow(self.comments_url, self.get_comments)        
 
     def get_comments(self,response):
@@ -42,9 +45,9 @@ class g1Spyder(scrapy.Spider):
         if number_of_pages == 0:
             print("Nothing to do here! Exiting...")
             exit(0)
-
-        number_of_pages = 2
+        
         # Request url comments for each page
+        number_of_pages = 2
         for i in range(1,number_of_pages): 
             self.log("Crawling...")
             yield response.follow(self.comments_url.replace('numero',"{}.json".format(i)), self.do_comments_analysis)
@@ -54,14 +57,19 @@ class g1Spyder(scrapy.Spider):
         self.log("Received file {}".format(received_file))
         ori_body = response.body.decode()
         jsonObj = self.body_to_json(ori_body)
-
-        # print(jsonObj)
+        
+        for i in range(len(jsonObj['itens'])):
+            replies = jsonObj['itens'][i]['replies']
+            userObj = jsonObj['itens'][i]['Usuario']
+            text = jsonObj['itens'][i]['texto']
+            
+            
 
     def body_to_json(self,body):
-        body = body.split('(')[1].split(')')[0]
-        body = literal_eval(body.replace('false', 'False')
+        # Convert body response to json
+        body = body.split('({')[1].split('})')[0]
+        body = eval("{"+body.replace('false', 'False')
         .replace('true','True')
-        .replace('null',"False")
-        .replace(" ","").rstrip())
+        .replace('null',"False")+"}")
 
         return body
